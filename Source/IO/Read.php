449482613df3f12d61348ab6a37884cb5f6eb221
Morteza <morteza@protonmail.com>
2022-09-18 15:05:17 +0200

    Add command function, support arguments when there is a command

--- Source/IO/Read.php
+++ Source/IO/Read.php
@@ -2,7 +2,28 @@
 
 namespace Saeghe\Cli\IO\Read;
 
-function argument($name, $default = null)
+function argument($name, $default = null): ?string
 {
-    return getopt('', [$name . '::'])[$name] ?? $default;
+    $input = getopt('', [$name . '::']);
+
+    if (count($input) === 0) {
+        global $argv;
+
+        $input = array_reduce($argv, function ($carry, $argument) use ($name) {
+            if (str_starts_with($argument, "--$name=")) {
+                return [$name => str_replace("--$name=", '', $argument)];
+            }
+
+            return $carry;
+        }, []);
+    }
+
+    return $input[$name] ?? $default;
+}
+
+function command(): ?string
+{
+    global $argv;
+
+    return str_starts_with($argv[1], '--') ? null : $argv[1];
 }
--- Tests/IO/ReadTest.php
+++ Tests/IO/ReadTest.php
@@ -6,15 +6,43 @@ test(
     title: 'it should read from input',
     case: function () {
         $email = 'my_email@saeghe.com';
-        $output = shell_exec(__DIR__ . '/../../TestRequirements/ReadHelper.php --email=' . $email);
-        assert($output === $email);
+        $output = shell_exec(__DIR__ . '/../../TestRequirements/ReadArgumentHelper.php --email=' . $email);
+        assert($output === $email, $output);
     }
 );
 
 test(
     title: 'it should return default value if argument not passed',
     case: function () {
-        $output = shell_exec(__DIR__ . '/../../TestRequirements/ReadHelper.php');
-        assert('default-email@saeghe.com' === $output);
+        $output = shell_exec(__DIR__ . '/../../TestRequirements/ReadArgumentHelper.php');
+        assert('default-email@saeghe.com' === $output, $output);
+    }
+);
+
+test(
+    title: 'it should read command',
+    case: function () {
+        $command = 'add';
+        $output = shell_exec(__DIR__ . '/../../TestRequirements/ReadCommandHelper.php ' . $command);
+        assert($output === $command, $output);
+    }
+);
+
+test(
+    title: 'it should return null as command when start with --',
+    case: function () {
+        $command = 'add';
+        $output = shell_exec(__DIR__ . '/../../TestRequirements/ReadCommandHelper.php --command=' . $command);
+
+        assert(is_null($output), $output);
+    }
+);
+
+test(
+    title: 'it should read argument from input after any command',
+    case: function () {
+        $email = 'my_email@saeghe.com';
+        $output = shell_exec(__DIR__ . '/../../TestRequirements/ReadArgumentHelper.php a-command --email=' . $email);
+        assert($output === $email, $output);
     }
 );
