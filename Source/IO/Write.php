d8899e82a09dd5e5cdaaef60f6770790f6ff1b8b
Morteza <morteza@protonmail.com>
2022-10-16 12:30:42 +0200

    Add assert_line, assert_success and assert_error

--- Source/IO/Write.php
+++ Source/IO/Write.php
@@ -2,17 +2,40 @@
 
 namespace Saeghe\Cli\IO\Write;
 
-function line(string $string)
+use \AssertionError;
+
+function line(string $string): void
 {
     echo "\e[39m$string" . PHP_EOL;
 }
 
-function success(string $string)
+function assert_line(string $expected, string $actual): bool
+{
+    $expected = "\e[39m$expected" . PHP_EOL;
+
+    return $actual === $expected ?: throw new AssertionError("Can not see '$expected' in '$actual'.");
+}
+
+function success(string $string): void
 {
     echo "\e[92m$string\e[39m" . PHP_EOL;
 }
 
-function error(string $string)
+function assert_success(string $expected, string $actual): bool
+{
+    $expected = "\e[92m$expected\e[39m" . PHP_EOL;
+
+    return $actual === $expected ?: throw new AssertionError("Can not see '$expected' in '$actual'.");
+}
+
+function error(string $string): void
 {
     echo "\e[91m$string\e[39m" . PHP_EOL;
+}
+
+function assert_error(string $expected, string $actual): bool
+{
+    $expected = "\e[91m$expected\e[39m" . PHP_EOL;
+
+    return $actual === $expected ?: throw new AssertionError("Can not see '$expected' in '$actual'.");
 }
--- Tests/IO/WriteTest.php
+++ Tests/IO/WriteTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\IO\WriteTest;
 
+use Saeghe\Cli\IO\Write;
+
 test(
     title: 'it should write message to output',
     case: function () {
@@ -9,6 +11,28 @@ test(
         $output = shell_exec(__DIR__ . '/../../TestRequirements/WriteHelper.php --function=line --message="' . $message . '"');
 
         assert($output === "\e[39m$message" . PHP_EOL, 'Line function does not work properly!');
+    }
+);
+
+test(
+    title: 'it should assert line message on output',
+    case: function () {
+        $message = 'This is a message to see on output.';
+        $output = shell_exec(__DIR__ . '/../../TestRequirements/WriteHelper.php --function=line --message="' . $message . '"');
+        assert(true === Write\assert_line($message, $output), 'assert_line is not working properly!' . $output);
+
+        $output = shell_exec(__DIR__ . '/../../TestRequirements/WriteHelper.php --function=line --message="' . $message . '"');
+        $expected = 'expected output';
+        $expectedOutput = "\e[39m$expected" . PHP_EOL;
+        $actualOutput = "\e[39m$message" . PHP_EOL;
+        try {
+            Write\assert_line($expected, $output);
+        } catch (\AssertionError $exception) {
+            assert(
+                "Can not see '$expectedOutput' in '$actualOutput'." === $exception->getMessage(),
+                'assert_line is not working properly!' . $output
+            );
+        }
     }
 );
 
@@ -22,10 +46,54 @@ test(
 );
 
 test(
+    title: 'it should assert success message on output',
+    case: function () {
+        $message = 'This is an success message to see on output.';
+        $output = shell_exec(__DIR__ . '/../../TestRequirements/WriteHelper.php --function=success --message="' . $message . '"');
+        assert(true === Write\assert_success($message, $output), 'assert_success is not working properly!' . $output);
+
+        $output = shell_exec(__DIR__ . '/../../TestRequirements/WriteHelper.php --function=success --message="' . $message . '"');
+        $expected = 'expected output';
+        $expectedOutput = "\e[92m$expected\e[39m" . PHP_EOL;
+        $actualOutput = "\e[92m$message\e[39m" . PHP_EOL;
+        try {
+            Write\assert_success($expected, $output);
+        } catch (\AssertionError $exception) {
+            assert(
+                "Can not see '$expectedOutput' in '$actualOutput'." === $exception->getMessage(),
+                'assert_error is not working properly!' . $output
+            );
+        }
+    }
+);
+
+test(
     title: 'it should write error message to output',
     case: function () {
         $message = 'This is an error message to see on output.';
         $output = shell_exec(__DIR__ . '/../../TestRequirements/WriteHelper.php --function=error --message="' . $message . '"');
         assert($output === "\e[91m$message\e[39m" . PHP_EOL, 'error function does not work properly!');
+    }
+);
+
+test(
+    title: 'it should assert error message on output',
+    case: function () {
+        $message = 'This is an error message to see on output.';
+        $output = shell_exec(__DIR__ . '/../../TestRequirements/WriteHelper.php --function=error --message="' . $message . '"');
+        assert(true === Write\assert_error($message, $output), 'assert_error is not working properly!' . $output);
+
+        $output = shell_exec(__DIR__ . '/../../TestRequirements/WriteHelper.php --function=error --message="' . $message . '"');
+        $expected = 'expected output';
+        $expectedOutput = "\e[91m$expected\e[39m" . PHP_EOL;
+        $actualOutput = "\e[91m$message\e[39m" . PHP_EOL;
+        try {
+            Write\assert_error($expected, $output);
+        } catch (\AssertionError $exception) {
+            assert(
+                "Can not see '$expectedOutput' in '$actualOutput'." === $exception->getMessage(),
+            'assert_error is not working properly!' . $output
+            );
+        }
     }
 );
